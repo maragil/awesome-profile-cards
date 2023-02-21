1ee7265171ab7df7f67615d7c455f49bfc7c7ec7
Patuco <patricia.beco.dg@gmail.com>
2023-02-21 13:35:47 +0100

    Optimize inputs event and fix reset button

--- src/js/02_preview.js
+++ src/js/02_preview.js
@@ -3,7 +3,6 @@
 /* eslint-disable strict */
 
 // Fill info into preview card //
-
 const data = {
   palette: 1,
   name: '',
@@ -16,42 +15,7 @@ const data = {
 };
 
 
-/*function handleInputName(event) {
-  event.preventDefault();
-  data.name = inputName.value;
-  updatePreview();
-}
-
-function handleInputJob(event) {
-  event.preventDefault();
-  data.job = inputJob.value;
-  updatePreview();
-}
-
-function handleInputTel(event) {
-  event.preventDefault();
-  data.phone = inputTel.value;
-  updatePreview();
-}
-
-function handleInputEmail(event) {
-  event.preventDefault();
-  data.email = inputEmail.value;
-  updatePreview();
-}
-
-function handleInputLinkedin(event) {
-  event.preventDefault();
-  data.linkedin = inputLinkedin.value;
-  updatePreview();
-}
-
-function handleInputGithub(event) {
-  event.preventDefault();
-  data.github = inputGithub.value;
-  updatePreview();
-}
-*/
+// Evento para escuchar a todos los inputs cuando se rellenan.
 const inputList = document.querySelectorAll('.js__input');
 
 function handleInputList(event) {
@@ -68,7 +32,9 @@ const updatePreview = () => {
     previewName.innerHTML = 'Nombre Apellido';
   } else {
     previewName.innerHTML = data.name;
-  } if (data.job === '') {
+  }
+
+  if (data.job === '') {
     previewJob.innerHTML = 'Front-end developer';
   } else {
     previewJob.innerHTML = data.job;
@@ -84,24 +50,11 @@ const updatePreview = () => {
   previewGithub.classList.remove("preview__card--link--disabled");
 };
 
-/*
-inputName.addEventListener('input', handleInputName);
 
-inputJob.addEventListener('input', handleInputJob);
-
-inputTel.addEventListener('input', handleInputTel);
-
-inputEmail.addEventListener('input', handleInputEmail);
-
-inputLinkedin.addEventListener('input', handleInputLinkedin);
-
-inputGithub.addEventListener('input', handleInputGithub);
-*/
 // Change colors palette //
 
 function handleClickPalettes(event) {
   const target = event.target.id;
-  /* const current = event.currentTarget; */
   if (target === "color_1") {
     previewCard.classList.remove("palette2");
     previewCard.classList.remove("palette3");
@@ -124,29 +77,29 @@ function handleClickPalettes(event) {
 designContent.addEventListener("click", handleClickPalettes);
 
 //botón reset
-
-const handleClickReset =(ev)=> {
+const handleClickReset = (ev) => {
   ev.preventDefault();
-  data.palette= 1;
-  data.name= '';
-  data.job= '';
-  data.phone= '';
-  data.email= '';
-  data.linkedin= '';
-  data.github= '';
-  data.photo= '';
-  palette1.checked= true;
-  inputName.value ='';
-  inputJob.value='';
-  inputEmail.value ='';
-  inputTel.value ='';
-  inputLinkedin.value ='';
-  inputGithub.value ='';
+  data.palette = 1;
+  data.name = "";
+  data.job = "";
+  data.phone = "";
+  data.email = "";
+  data.linkedin = "";
+  data.github = "";
+  data.photo = "";
+  palette1.checked = true;
+  inputName.value = "";
+  inputJob.value = "";
+  inputEmail.value = "";
+  inputTel.value = "";
+  inputLinkedin.value = "";
+  inputGithub.value = "";
   previewCard.classList.remove("palette2");
   previewCard.classList.remove("palette3");
   previewCard.classList.add("palette1");
+  profileImage.style.backgroundImage = data.photo;
+  profilePreview.style.backgroundImage = "";
   updatePreview();
 };
 
-
-btnReset.addEventListener('click', handleClickReset);
+btnReset.addEventListener("click", handleClickReset);
