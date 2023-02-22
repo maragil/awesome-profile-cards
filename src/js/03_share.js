f98946d340c0ce7a912f8e0b630b9face6bb87e0
Nuria Castaño <codesalnuria@gmail.com>
2023-02-22 17:29:55 +0100

    add fetch function and twitter button share

--- src/js/03_share.js
+++ src/js/03_share.js
@@ -1,25 +1,31 @@
 'use strict';
 
 const sendBtn = document.querySelector('.js_create_button');
+const infoText = document.querySelector('.js_infoText');
+const btnTwitter = document.querySelector('.js_buttonTwitter');
+const shareLink = document.querySelector('.js_shareLink');
+const shareLinkTwitter = document.querySelector('.js_shareLinkTwitter');
+
 function handleClickSend(event){ //dentro de esta función tiene q ocurrir el FETCH, para q cuando la usuaria haga click los datos se envíen
     event.preventDefault();
-    fetch(
-        'https://dev.adalab.es/api/card/',//estos 3 parámetros se 'tunean'
+    fetch('https://dev.adalab.es/api/card/',
         {
             method: 'POST',
             headers:{'content-type': 'application/json'},
-            body: JSON.stringify(data), //Hay q tranformar el objeto data en string
+            body: JSON.stringify(data),
         })
         .then(response => response.json())
         .then((data) =>{
+            console.log(data);
             if( ! data.success){ 
-                
-                
-                //si este success es false, la negación lo convierte a true
-                //if(data.success === false){
-                //monstrar mensaje a usuaria cuando success es FALSE
+                infoText.innerHTML = data.error;
+                shareLink.href='';
             }else{
-                //mostar la dirección y el btn de Tw.Si succes es true, la negación lo convierte a false y saldría este sms
+                infoText.innerHTML="La tarjeta ha sido creada";
+                shareLink.classList.remove('collapsed');
+                shareLink.href = data.cardURL;
+                shareLink.innerHTML = data.cardURL;
+                btnTwitter.classList.remove('collapsed');
             }
         });
 }
